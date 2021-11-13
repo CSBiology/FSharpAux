66af4c103abcface8f73c35b09551ca90ab83c49
omaus <omaus@bio.uni-kl.de>
2021-11-14 00:27:22 +0100

    Finish SeqTests :white_check_mark:

--- tests/FSharpAux.Tests/SeqTests.fs
+++ tests/FSharpAux.Tests/SeqTests.fs
@@ -9,19 +9,47 @@ let testSeq3                            = seq {3; 3; 2; 4; 1; 1}
 let testSeq4                            = seq {3; 3; 2; 4; 2; 2}
 let testSeq5                            = seq {3; 3; 2; 4; 2; 1}
 
-let testSeq2_groupWhen_Equal            = seq {seq {1337; 14}; seq {23}; seq {23}; seq {69}; seq {1; 2}; seq {3; 1000}; seq {9001; 23}}
-let testSeq2_groupWhen_NotEqual         = seq {seq {1337; 14}; seq {23}; seq {23}; seq {69}; seq {1; 2}; seq {3; 1000}; seq {9001; 23}}
+let testSeq2_groupWhen_Equal            = seq {seq {3}; seq {3; 2; 4}; seq {1; 2}}
+let testSeq2_groupWhen_NotEqual         = seq {seq {3}; seq {3; 2; 4}; seq {1}; seq {2}}
+let testSeq3_groupWhen_Equal            = seq {seq {3}; seq {3; 2; 4}; seq {1}; seq {1}}
+let testSeq3_groupWhen_NotEqual         = seq {seq {3}; seq {3; 2; 4}; seq {1; 1}}
+let testSeq4_groupWhen_Equal            = seq {seq {3}; seq {3; 2; 4; 2; 2}}
+let testSeq4_groupWhen_NotEqual         = seq {seq {3}; seq {3; 2; 4; 2}; seq {2}}
+let testSeq5_groupWhen_Equal            = seq {seq {3}; seq {3; 2; 4; 2}; seq {1}}
+let testSeq5_groupWhen_NotEqual         = seq {seq {3}; seq {3; 2; 4; 2; 1}}
+
+// helper functions
+let list s = Seq.toList s
+let list2 s = Seq.map (Seq.toList) s |> Seq.toList
 
 [<Tests>]
 let seqTests =
     testList "SeqTests" [
         let isOdd = fun n -> n % 2 <> 0
         testList "Seq.groupWhen" [
-            testCase "returns correct list" (fun _ ->
-                Expect.equal (testSeq1 |> Seq.groupWhen isOdd) testSeq1_groupWhen_Equal "Seq.groupWhen did return correct jagged sequence"
+            testCase "returns correct jagged list, case1: [3; 3; 2; 4; 1; 2]" (fun _ ->
+                Expect.equal (testSeq2 |> Seq.groupWhen isOdd |> list2) (testSeq2_groupWhen_Equal |> list2) "Seq.groupWhen did return correct jagged list"
             )
-            testCase "returns incorrect list" (fun _ ->
-                Expect.notEqual (testSeq1 |> Seq.groupWhen isOdd) testList1_filteri_NotEqual "Seq.groupWhen did not return incorrect jagged sequence"
+            testCase "returns incorrect jagged list, case1: [3; 3; 2; 4; 1; 2]" (fun _ ->
+                Expect.notEqual (testSeq2 |> Seq.groupWhen isOdd |> list2) (testSeq2_groupWhen_NotEqual |> list2) "Seq.groupWhen did not return incorrect jagged list"
+            )
+            testCase "returns correct jagged list, case2: [3; 3; 2; 4; 1; 1]" (fun _ ->
+                Expect.equal (testSeq3 |> Seq.groupWhen isOdd |> list2) (testSeq3_groupWhen_Equal |> list2) "Seq.groupWhen did return correct jagged list"
+            )
+            testCase "returns incorrect jagged list, case2: [3; 3; 2; 4; 1; 1]" (fun _ ->
+                Expect.notEqual (testSeq3 |> Seq.groupWhen isOdd |> list2) (testSeq3_groupWhen_NotEqual |> list2) "Seq.groupWhen did not return incorrect jagged list"
+            )
+            testCase "returns correct jagged list, case3: [3; 3; 2; 4; 2; 2]" (fun _ ->
+                Expect.equal (testSeq4 |> Seq.groupWhen isOdd |> list2) (testSeq4_groupWhen_Equal |> list2) "Seq.groupWhen did return correct jagged list"
+            )
+            testCase "returns incorrect jagged list, case3: [3; 3; 2; 4; 2; 2]" (fun _ ->
+                Expect.notEqual (testSeq4 |> Seq.groupWhen isOdd |> list2) (testSeq4_groupWhen_NotEqual |> list2) "Seq.groupWhen did not return incorrect jagged list"
+            )
+            testCase "returns correct jagged list, case4: [3; 3; 2; 4; 2; 1]" (fun _ ->
+                Expect.equal (testSeq5 |> Seq.groupWhen isOdd |> list2) (testSeq5_groupWhen_Equal |> list2) "Seq.groupWhen did return correct jagged list"
+            )
+            testCase "returns incorrect jagged list, case4: [3; 3; 2; 4; 2; 1]" (fun _ ->
+                Expect.notEqual (testSeq5 |> Seq.groupWhen isOdd |> list2) (testSeq5_groupWhen_NotEqual |> list2) "Seq.groupWhen did not return incorrect jagged list"
             )
         ]
     ]
